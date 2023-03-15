a3d375c98ec85a1d59e6dda8d6db1b2314a76e75
Chris Pierson <cpierswim@gmail.com>
2023-03-15 16:01:43 -0400

    New: added some skelton code

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import requests
-import json
-from song import Song
-from helper import Helper
+from music_library_interface import MusicLibraryInterface
 
-
-response = requests.get("http://127.0.0.1:5000/api/songs")
-
-test1 = response.content
-songs_json = response.json()
-
-song_list = []
-for song in songs_json:
-    song_list.append(Song.song_decoder(song))
-
-
-Helper.display_songs(song_list)
-
-
-pass
+interface = MusicLibraryInterface()
+interface.run
---
+++ music_library_interface.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from song import Song
+from helper import Helper
+
+
+
+
+
+class MusicLibraryInterface():
+
+    def run(self):
+        response = requests.get("http://127.0.0.1:5000/api/songs")
+
+        test1 = response.content
+        songs_json = response.json()
+
+        song_list = []
+        for song in songs_json:
+            song_list.append(Song.song_decoder(song))
+
+
+        Helper.display_songs(song_list)
